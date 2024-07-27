46a8276e02cbc0aa37ba4a66f5dfed17b6497044
V01Ddev <x.v.0.1.d.xhacking@gmail.com>
2024-07-28 00:55:25 +0300

    Migrated to express.js, this is not functional and sucks ass

--- public/blog/scripts/script.js
+++ public/blog/scripts/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var title = $(this).find("h2").text();
         title = title.replace(" ", "_");
         dir = title;
-        title = title + ".php";
+        title = title + ".html";
         url = "/blog/content/" + dir + "/" + title;
         location.href=url;
     });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -17,29 +17,6 @@ console.log("⣿⣿⣿⣶⣮⣭⣁⣒⣒⣒⠂⠠⠬⠭⠭⠭⢀⣀⣠⣄⡘⠿�
 console.log("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡈⢿⣿⣿⣿⣿⣿")
 
 
-
-function myFunction() {
-  /* Get the text field */
-  var copyText = document.getElementById("myInput");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-   /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
-
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
-
-  let svgLogo = document.getElementById("logo_svg");
-  console.log(svgLogo)
-
-  svgLogo.style.opacity = 1;
-} 
-
-
-
 var i = 0;
 var txt = "I am v01d, welcome to my website"
 var speed = 100;
